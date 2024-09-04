f20c6d6c94c41faaa6c8f8f405e31eacbc52dd12
dagou <eric9n@gmail.com>
2024-09-04 17:07:27 +0800

    gtdb-tree

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod node;
+pub mod tree;
+
+#[cfg(feature = "python")]
+mod python;
+
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python")]
+#[pymodule]
+fn gtdb_tree(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(python::parse_tree, m)?)?;
+    m.add_class::<python::Node>()?;
+    Ok(())
+}
