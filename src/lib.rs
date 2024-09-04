835f4b727fb4b37923b77ddfcefcdeb8fa0cc86f
dagou <eric9n@gmail.com>
2024-09-04 19:51:34 +0800

    PyO3

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use pyo3::prelude::*;
 
 #[cfg(feature = "python")]
 #[pymodule]
+#[pyo3(name = "gtdb_tree")]
 fn gtdb_tree(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(python::parse_tree, m)?)?;
     m.add_class::<python::Node>()?;
